f2acebaced02184a746835cfda982b6536371252
RehnumaTasnim71 <rehnumatasnim71@gmail.com>
2025-05-12 23:22:19 +0800

    helloworld

---
+++ inputfromuser.py
@@ -0,0 +1,11 @@
+x = int(input('take a number: '))
+print('number: ', x)
+print(type(x))
+
+
+age = 45
+height = 5.4
+is_student = False
+
+print(age,"\n",height,"\n",is_student) #space print kore
+print(f"{age}\n{height}\n{is_student}") #space count kore na

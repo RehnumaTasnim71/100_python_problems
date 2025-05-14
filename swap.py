c910dc65200f764e210e87506561d404ebe64da2
RehnumaTasnim71 <rehnumatasnim71@gmail.com>
2025-05-14 17:09:45 +0800

    swap

---
+++ swap.py
@@ -0,0 +1,10 @@
+a = int(input("First number a: "))
+b = int(input("2nd number b: "))
+# x = a
+# a = b
+# b = x 
+
+a,b = b,a # different type of swap
+
+# print("after swap: ", a, b)
+print("after swap 2: a=",a, "b=", b) 
